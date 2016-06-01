c11ecc06d1b185241857b4742d89776e9fb65dde
Jelynn <jelynn.tang@clever-m.com>
2016-06-01 11:32:22 +0800

    修改

--- src/main/java/com/myee/niuroumian/domain/OrderInfo.java
+++ src/main/java/com/myee/niuroumian/domain/OrderInfo.java
@@ -1,8 +1,6 @@
 package com.myee.niuroumian.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -12,18 +10,38 @@ import java.sql.Timestamp;
  * Created by Jelynn on 2016/6/1.
  */
 @Entity
-@Table(name="order_info")
-public class OrderInfo implements Serializable{
+@Table(name = "order_info")
+public class OrderInfo implements Serializable {
 
     @Id
+    @Column(name = "order_id")
+    @TableGenerator(name = "PkGen_120", table = "ad_sequence", pkColumnName = "name", pkColumnValue = "R_order_info", valueColumnName = "currentnextsys", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "PkGen_120")
+    private Long orderId; //订单ID
 
-    private Long orderId;
-    private String orderState;
-    private String payState;
-    private String goodsId;
-    private String goodsName;
-    private BigDecimal price;
-    private Timestamp orderDate;
+    @Column(name = "shop_id")
+    private Long shopId;   //商铺ID
+
+    @Column(name = "user_id")
+    private Long userId;    //用户ID
+
+    @Column(name = "dish_id")
+    private Long dishId;    //商品ID
+
+    @Column(name = "create_time")
+    private Timestamp createTime;   //创建时间
+
+    @Column(name = "update_time")
+    private Timestamp updateTime; //更新时间
+
+    @Column(name = "order_time")
+    private String orderState; //订单状态
+
+    @Column(name = "order_price")
+    private BigDecimal orderPrice;  //订单金额
+
+    @Column(name = "pay_state")
+    private String payState;  //支付状态
 
     public Long getOrderId() {
         return orderId;
@@ -31,6 +49,46 @@ public class OrderInfo implements Serializable{
 
     public void setOrderId(Long orderId) {
         this.orderId = orderId;
+    }
+
+    public Long getShopId() {
+        return shopId;
+    }
+
+    public void setShopId(Long shopId) {
+        this.shopId = shopId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getDishId() {
+        return dishId;
+    }
+
+    public void setDishId(Long dishId) {
+        this.dishId = dishId;
+    }
+
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    public Timestamp getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Timestamp updateTime) {
+        this.updateTime = updateTime;
     }
 
     public String getOrderState() {
@@ -41,43 +99,19 @@ public class OrderInfo implements Serializable{
         this.orderState = orderState;
     }
 
+    public BigDecimal getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(BigDecimal orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
     public String getPayState() {
         return payState;
     }
 
     public void setPayState(String payState) {
         this.payState = payState;
-    }
-
-    public String getGoodsId() {
-        return goodsId;
-    }
-
-    public void setGoodsId(String goodsId) {
-        this.goodsId = goodsId;
-    }
-
-    public String getGoodsName() {
-        return goodsName;
-    }
-
-    public void setGoodsName(String goodsName) {
-        this.goodsName = goodsName;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Timestamp getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(Timestamp orderDate) {
-        this.orderDate = orderDate;
     }
 }
